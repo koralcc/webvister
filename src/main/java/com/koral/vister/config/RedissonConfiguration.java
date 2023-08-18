21fd5be53b2f725c51668f381c4c7baca20bc362
koral <376501446@qq.com>
2023-08-18 10:57:05 +0800

    添加分布式锁支持

---
+++ src/main/java/com/koral/vister/config/RedissonConfiguration.java
@@ -0,0 +1,57 @@
+package com.koral.vister.config;
+
+import org.redisson.Redisson;
+import org.redisson.config.Config;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Redisson配置
+ */
+@ConfigurationProperties(
+        prefix = "spring.redis"
+)
+@Configuration
+public class RedissonConfiguration {
+
+    private String host;
+    private String port;
+    private String password;
+
+    @Bean(destroyMethod="shutdown")
+    Redisson redisson(){
+        String address = "redis://"+host+":"+port;
+        Config config = new Config();
+        config.useSingleServer().setAddress(address)
+                .setConnectionPoolSize(30)
+                .setConnectionMinimumIdleSize(10)
+                .setPassword(password)
+                .setDatabase(8);
+        return (Redisson) Redisson.create(config);
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public String getPort() {
+        return port;
+    }
+
+    public void setPort(String port) {
+        this.port = port;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
--- src/main/java/com/koral/vister/controller/TestController.java
+++ src/main/java/com/koral/vister/controller/TestController.java
@@ -1,14 +1,23 @@
 package com.koral.vister.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.redisson.Redisson;
+import org.redisson.api.RLock;
 import org.springframework.core.env.Environment;
 import org.springframework.data.redis.core.HyperLogLogOperations;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
 
-@RestController
+@Controller
+@Slf4j
 public class TestController {
     @Resource(name = "stringRedisTemplate")
     private StringRedisTemplate redisTemplate;
@@ -16,10 +25,43 @@ public class TestController {
     @Resource
     private Environment environment;
 
+    @Resource
+    private Redisson redisson;
 
     @GetMapping("/index")
+    @ResponseBody
     public Long index() {
-        HyperLogLogOperations hyperLogLogOperations = redisTemplate.opsForHyperLogLog();
+        HyperLogLogOperations<String,String> hyperLogLogOperations = redisTemplate.opsForHyperLogLog();
         return hyperLogLogOperations.size(environment.getProperty("spring.application.name"));
+    }
+
+
+    @GetMapping("/modelandview")
+    public ModelAndView modelandview() {
+        return new ModelAndView("hello");
+
+    }
+
+    @PostMapping("/alipay")
+    @ResponseBody
+    public ResponseEntity<String> alipay() {
+        // validate
+        RLock lock = redisson.getLock("alipaylock:orderid");
+        try {
+            boolean b = lock.tryLock(0, 30, TimeUnit.SECONDS);
+            if (!b) {
+                log.warn("分布式锁占用");
+                return ResponseEntity.status(500).body("正在执行，请勿频繁操作");
+            }
+            log.info("执行alipay支付逻辑");
+            return ResponseEntity.ok("hello");
+        } catch (InterruptedException e) {
+            log.error("分布式锁获取异常");
+            return ResponseEntity.status(500).body("分布式锁获取失败");
+        } finally {
+            if (lock.isHeldByCurrentThread()) {
+                lock.unlock();
+            }
+        }
     }
 }
