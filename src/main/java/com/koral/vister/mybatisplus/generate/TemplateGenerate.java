344d35662ce04eeeb8b603e9cd3fd23ce2c12a20
陈建云 <376501446@qq.com>
2024-07-23 11:13:44 +0800

    oracle代码生成支持

--- src/main/java/com/koral/vister/mybatisplus/generate/TemplateGenerate.java
+++ src/main/java/com/koral/vister/mybatisplus/generate/TemplateGenerate.java
@@ -10,8 +10,9 @@ import java.util.Collections;
 
 public class TemplateGenerate {
     public static void main(String[] args) {
-        FastAutoGenerator.create("jdbc:mysql://10.6.15.15/join_ucenter?useSSL=false&useUnicode=true&characterEncoding=UTF8&serverTimezone=Asia/Shanghai", "u_dev", "8APGj4Nk7SCxDYQBPUrY")
-                .globalConfig(builder -> {
+//        FastAutoGenerator.create("jdbc:mysql://10.6.15.15/join_date_temp?useSSL=false&useUnicode=true&characterEncoding=UTF8&serverTimezone=Asia/Shanghai", "u_temp_data", "zZqCT9$*nEKQ")
+          FastAutoGenerator.create("jdbc:oracle:thin:@10.6.4.152:1521:orcl?characterEncoding=UTF-8&NCHAR_CHARACTERSET=ZHS16GBK", "test", "test123")
+        .globalConfig(builder -> {
                     builder.author("koral") // 设置作者
                             .enableSwagger() // 开启 swagger 模式
                             .fileOverride() // 覆盖已生成文件
@@ -31,8 +32,8 @@ public class TemplateGenerate {
                             .pathInfo(Collections.singletonMap(OutputFile.xml, "D:\\softwares\\gdx\\webvisitercount\\src\\main\\resources\\com\\koral\\vister\\server\\mapper")); // 设置mapperXml生成路径
                 })
                 .strategyConfig(builder -> {
-                    builder.addInclude("uc_todo_record")// 设置需要生成的表名
-                            .addTablePrefix("t_", "c_"); // 设置过滤表前缀
+                    builder.addInclude("TB_GOS_VOUCHER_ZDGB_DIVISION_RELATION");// 设置需要生成的表名
+//                            .addTablePrefix("t_", "c_"); // 设置过滤表前缀
                 })
                 .templateEngine(new FreemarkerTemplateEngine()) // 使用Freemarker引擎模板，默认的是Velocity引擎模板
                 .execute();
